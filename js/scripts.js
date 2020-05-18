a89a94e1f9c78dd49039942f0a3182c4ca11daad
virginiah2020 <vancyvy254@gmail.com>
2020-05-18 16:59:23 +0300

    modified the ui for jquery code

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,14 @@ $(document).ready(function(){
         $("#product").toggle();  
     });
     });
+    $("button").click(function (omonge) {
+        var client = document.getElementById('nameDetail').value;
+        alert(' Hello' + ' client ' + client + ' we have received your message . ' + ' Reach out to us anytime');
+        wamuyu.preventDefault();
+      });
+    
+      $("button").on('click', function () {
+        $('form').each(function () {
+          this.reset();
+        });
+      });
